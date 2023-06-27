b8527826fd250946b7fa0f8bf5f360f829077045
SF <Sabriaford23@gmail.com>
2023-06-27 11:00:48 -0700

    added imports to routes

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
 from app.models.card import Card
+from flask import jsonify
+from flask import abort
 
 cards_bp = Blueprint("cards", __name__, url_prefix="/cards")
 
