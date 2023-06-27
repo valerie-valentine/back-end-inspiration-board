b91b8ff9d0b59bcb4a81b75c28eee98faf5a46ff
SF <Sabriaford23@gmail.com>
2023-06-27 10:58:02 -0700

    created first route create new card

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,18 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
+from app.models.card import Card
 
-# example_bp = Blueprint('example_bp', __name__)
+cards_bp = Blueprint("cards", __name__, url_prefix="/cards")
+
+# create a new card
+@cards_bp.route("", methods =["POST"])
+def create_card():
+    request_body = request.get_json()
+    new_card = Card(
+        body=request_body["body"],
+        likes=request_body["likes"])
+
+    db.session.add(new_card)
+    db.session.commit()
+
+    return make_response(f"Card {new_card.body} created")
