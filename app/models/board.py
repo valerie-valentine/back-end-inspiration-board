d7ea13d1feffda6ce2229962acea543aa2677744
Virginia Ramos <virginia.rare@gmail.com>
2023-07-18 15:42:58 -0500

    removing lines 11 and 16

--- app/models/board.py
+++ app/models/board.py
@@ -8,12 +8,10 @@ class Board(db.Model):
 
 
     def to_dict(self):
-        # cards_dict= [card.to_dict() for card in self.cards]
         return{
             "id":self.board_id,
             "title":self.title,
             "owner":self.owner,
-            # "cards":cards_dict 
         }
 
     @classmethod
