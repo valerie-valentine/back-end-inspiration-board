00e604dd11a2716722dcb92557ebc5a415ff3411
Valerie Valentin <valerie.valentine93@gmail.com>
2023-07-20 00:51:11 -0700

    Added image key-value to board to-dict function

--- app/models/board.py
+++ app/models/board.py
@@ -4,6 +4,7 @@ class Board(db.Model):
     board_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     owner = db.Column(db.String)
+    image = db.Column(db.String)
     cards = db.relationship("Card", back_populates="board")
 
 
@@ -12,13 +13,15 @@ class Board(db.Model):
             "id":self.board_id,
             "title":self.title,
             "owner":self.owner,
+            "image": self.image
         }
 
     @classmethod
     def from_dict(cls, board_data):
         new_board = Board(
             title=board_data["title"],
-            owner=board_data["owner"]
+            owner=board_data["owner"],
+            image=board_data["image"]
         )
         return new_board
 
