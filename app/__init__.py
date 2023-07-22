fd96f9069c60dc4453fb9cc6ced1349e24851627
Valerie Valentin <valerie.valentine93@gmail.com>
2023-07-21 17:19:18 -0700

    Change to connection string to render database

--- app/__init__.py
+++ app/__init__.py
@@ -17,8 +17,8 @@ def create_app(test_config=None):
 
     # app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
     #     "SQLALCHEMY_DATABASE_URI")
-    # # app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-    # #     "RENDER_DATABASE_URI")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+        "RENDER_DATABASE_URI")
     
     if test_config is None:
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
